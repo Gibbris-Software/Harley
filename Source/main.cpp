e1ccb8b0a4c612379bd3ea2fac3572e124c989a6
John Hiatt <jhia186034@adams12.org>
2017-04-18 20:37:01 -0600

    Updated main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <SDL2/SDL.h>
 
 void main(){
-    std::cout << "It works!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
