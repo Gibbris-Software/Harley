89b5aa4d12f986fdef0ceaf1fc82095420fc706c
John Hiatt <jhia186034@adams12.org>
2017-04-18 20:40:52 -0600

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 
-void main(){
+int main(int argc, char* argv[]){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     SDL_Delay(2000);
